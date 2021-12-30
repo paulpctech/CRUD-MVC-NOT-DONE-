f0b93ba76229157c8f19a61697a8dfe256159f88
Paul Clarence <Paul Clarence@DESKTOP-RBAD5RE>
2021-12-31 03:25:34 +0800

    new push

---
+++ Controllers/VolunteerController.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+using System.Collections;
+using System.Diagnostics;
+
+namespace WebApplication1.Controllers
+{
+    public class VolunteerController : Controller
+    {
+        // GET: Volunteer
+        public ActionResult Index()
+        {
+            ExamDbEntities2 model = new ExamDbEntities2();
+            return View(model.VolunteerLists.ToList());
+
+        }
+
+        // GET: Volunteer/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Volunteer/Create
+        public ActionResult Create()
+        {
+            
+            return View();
+
+        }
+
+        // POST: Volunteer/Create
+        [HttpPost]
+        public ActionResult Create(VolunteerList volunteers)
+        {
+            try
+            {
+                using (ExamDbEntities2 model = new ExamDbEntities2()) 
+                {
+                    model.VolunteerLists.Add(volunteers);
+                    model.SaveChanges();
+
+                }
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Volunteer/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Volunteer/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Volunteer/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Volunteer/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
